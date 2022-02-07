48d0d2241d3dfb0fb726af3ce1f7e44b3085a83b
Brendan Bischoff <brendanbischoff@gmail.com>
2022-02-07 12:05:44 -0500

    added develop branch

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+function initApp() {
+    startHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+}])}
+
+initApp();
